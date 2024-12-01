eeba8a58caaae123c47ad73765fb956c580d5cbf
Akila-kumara <nakilakumara@gmail.com>
2024-12-01 17:35:39 +0530

    install dotenv

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 import productRouter from "./routes/productRouter.js";
 import userRouter from "./routes/userRouter.js";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
