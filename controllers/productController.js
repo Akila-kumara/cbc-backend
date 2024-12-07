a6d605a71ca4e444ecf6877f29c7b276a71b7204
Akila-kumara <nakilakumara@gmail.com>
2024-12-07 10:41:17 +0530

    create product Router

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,34 @@
 import Product from "../models/product.js";
 
+import {isAdmin} from "./userController.js";
+
 export function createProduct(req,res){
-    
+    if(!isAdmin(req)) {
+        req.json({
+            message :" Please login as administrator to add products"
+        })
+        return
+    }
+
+    const newProductData = req.body
+
+    const product = new Product(newProductData)
+
+    product.save().then(()=>{
+        res.json({
+            message : "Product Details Created Successfully"
+        })
+    }).catch((error)=>{
+        res.json({
+            message : error
+        })
+    }
+
+    )
+}
+
+export function getProducts(req,res){
+    Product.find({}).then((products)=>{
+        res.json(products)
+    })
 }
